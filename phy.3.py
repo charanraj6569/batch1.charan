db1ef11e22d4db9d4d2e0caa7f1756967316be2a
charanraj6569 <163230204+charanraj6569@users.noreply.github.com>
2024-03-13 03:44:22 -0700

    Add files via upload

---
+++ phy.3.py
@@ -0,0 +1,165 @@
+13/3/2024
+day:3
+# ? Take values of length and breadth of a
+# ? from user and check if it is square or not
+# if length==int(input())
+   breadth=(int())
+     print("its asqure")
+     else:
+         print("its not square")
+Eg:4
+       python program to check whether the
+         given integer is a multiple of both 5and 7
+
+ n=int(input("enter the number"))
+ if n%5==0 and n%7==0
+ print("yes")
+ else:
+     print("no")
+ Eg:5
+ write a program to accept the cost price of a bike and display the road tax to be paid
+ according to the following criteria:
+ cost price (in Rs)                     tax
+ >100000                                15 % + discount 6%
+ >50000 and <=100000                     10%
+ <=50000                                 5%
+ price=int(intput("enter the price :"))
+ amountn =0
+  if price >=100000*(6/100)
+  value = price - discount
+  amount=value*(15/100)
+  print(value+amount)
+else:
+    tax = price *(5/100)
+    total = price+tax
+    print("the on road cost of bike is :",total)
+
+
+# !--------> if else
+Eg:1
+a = 7
+b =9
+c = 3
+if a>b and a>c:
+    print ("A is greater")
+elif b>a and b>c
+    print("B is greater")
+else:
+    print("c is greater
+  
+A school has follwing rules for grading system:
+  a.beiow 25 - F
+  b.25 to 45 - E        
+  c.45 to 50 - D
+  d.50 to 60 - c
+  e. 60 to 80 -A
+   Ask user to enter marks and print the corresponding grade.+
+          mark =int (intput("Enter mark:"))
+          if
+
+
+
+
+
+
+
+# !------->short hand if else
+          Eg: 1
+          a =9
+          b= 90
+          if a>b:
+          print("A")
+          else:
+          print("B")
+          ------>using short hand if else
+          Rules
+          1.)statement inside the if condition to have be present at first
+          2.)elif cannot be used in short hand if else
+          3.)Always it have to end with else
+
+print("A") if a>b else print ("B")
+
+
+code to check the given is vowel or consonent
+char =input("Enter char:")
+if char =="a" or char=='e' or char=='i' or char=='o' or char=='u'
+print("is a vowel")
+else:
+    print("its consonent")
+
+    ?or
+    strl = "aeiouAEIOPU"
+    if char in strl:
+        print("vowel")
+        else:
+            print("consonent")
+
+short hand if else
+char = input ("enter the char:")
+strl="aeiouAEIOU"
+print("vowels") if char in strl else print("consonent")
+
+
+----->elif ladder usihg short hand if else
+Eg:1
+Find greater among 3 variables using short hand if else
+a = 8
+b = 5
+c = 9
+print("A is greater") if a>b and a>c else print("B is greater")
+if b>a and b>C else print("C is greater")
+
+!------->looping
+for loop is used for iteartion, if we know tha number of times the loop have to run
+it is used to iterate the iterable eg(string ,list,tuple, etc....)
+
+------>syntex for loop
+
+for syntex in c
+for(i=0,i<10 ;i++){
+    printf("hello");
+    }
+for syntax in python
+for user defined variable in range(start,end,step):default step value is 1
+statement
+statement
+statement
+Eg:1
+To print the value from 
+
+
+
+ for val in range (21,43,1):
+     print(val)no output
+
+Eg:5
+break----> used to teerminate the loop
+
+for val in range (1,20):
+    if val ==10:
+        break
+    print(val)
+
+Eg:7
+for val in range(1,10)
+if val==6:
+    if val==6
+    break
+
+Eg:1
+for val in range(1,10):
+    if val==5:
+        continue
+    print(val)
+
+for val in range(1,10):
+    if val==5:
+        continue
+    print(val)
+
+
+        
+          
+
+
+          
